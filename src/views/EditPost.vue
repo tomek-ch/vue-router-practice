1cd8d72bf370e64850df3e58204e12c3b7b8a8de
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-18 15:55:27 +0200

    Handle post edit not found

--- src/views/EditPost.vue
+++ src/views/EditPost.vue
@@ -1,15 +1,22 @@
 <template>
-  <PostForm btnLabel="Update" @submit="edit" :initial="post"/>
+  <PostForm
+    v-if="post"
+    btnLabel="Update"
+    @submit="edit"
+    :initial="post"
+  />
+  <NotFound v-else/>
 </template>
 
 <script>
 import PostForm from '../components/PostForm';
+import NotFound from './NotFound';
 
 export default {
   name: 'EditPost',
   emits: ['edit-post'],
   props: ['posts'],
-  components: { PostForm },
+  components: { PostForm, NotFound },
   data() {
     return {
       post: this.posts.find(({ id }) => id === Number(this.$route.params.id)),
