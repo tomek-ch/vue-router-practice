824519aa19e2ff615523e5baa72fc8f9e5ea3b8d
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-18 15:53:39 +0200

    Handle post not found

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
+  <div v-if="post">
     <h1>{{ post.title }}</h1>
     <p>{{ post.body }}</p>
+  </div>
+  <div v-else>
+    <NotFound/>
   </div>
 </template>
 
 <script>
+import NotFound from './NotFound';
+
 export default {
   name: 'PostList',
   props: ['posts'],
+  components: { NotFound },
   data() {
     return {
       post: this.posts.find(({ id }) => id === Number(this.$route.params.id)),
